048c96f9e316e0e39a3b44782dedfe52fbcbbb7d
korilin <korilin.dev@gmail.com>
2023-10-08 19:41:43 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.jvm) apply false
 }
 
 group = providers.gradleProperty("group").get()
