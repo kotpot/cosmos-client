99befee8909e7f3b9b890607e0c0dbc2de7c5d3b
korilin <korilin.dev@gmail.com>
2023-10-08 15:55:49 +0800

    Add content package

--- desktop/src/main/kotlin/ui/main/HomeContent.kt
+++ desktop/src/main/kotlin/ui/main/HomeContent.kt
@@ -1,4 +1,8 @@
+<<<<<<<< HEAD:desktop/src/main/kotlin/ui/page/HomePage.kt
 package org.kotpot.cosmos.desktop.ui.page
+========
+package org.kotpot.cosmos.desktop.ui.main
+>>>>>>>> 2b71804 (Add content package):desktop/src/main/kotlin/ui/main/HomeContent.kt
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
@@ -6,7 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun HomePage(
+fun HomeContent(
     modifier: Modifier = Modifier
 ) {
     Column(
---
+++ desktop/src/main/kotlin/ui/main/LibraryContent.kt
@@ -0,0 +1,17 @@
+package org.kotpot.cosmos.desktop.ui.main
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LibraryContent(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier
+    ) {
+        Text(text = "Hello, world!")
+    }
+}
