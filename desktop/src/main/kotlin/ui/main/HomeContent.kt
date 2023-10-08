c43cda94b29eca45afc70f38b5cb844151d9ad95
korilin <korilin.dev@gmail.com>
2023-10-08 18:33:25 +0800

    * fix merge error

--- desktop/src/main/kotlin/ui/main/HomeContent.kt
+++ desktop/src/main/kotlin/ui/main/HomeContent.kt
@@ -1,8 +1,4 @@
-<<<<<<<< HEAD:desktop/src/main/kotlin/ui/page/HomePage.kt
-package org.kotpot.cosmos.desktop.ui.page
-========
 package org.kotpot.cosmos.desktop.ui.main
->>>>>>>> 2b71804 (Add content package):desktop/src/main/kotlin/ui/main/HomeContent.kt
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
